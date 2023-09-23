28262b41adc562c0feba8e2abbe348f608fe6cd5
Hyeyeon Seo <42148741+skojphy@users.noreply.github.com>
2023-09-24 05:54:55 +0900

    :rocket: 빌드 테스트

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,31 +1,29 @@
 import { database } from '$db/database';
 
 export const load = async function () {
-	console.log('>>>> start');
-	const data = await database
-		.find(
-			{},
-			{
-				projection: {
-					_id: String,
-					title: String,
-					state: Number
-				}
-			}
-		)
-		.toArray();
-
-	data.forEach((item) => {
-		console.log('>>>> title : ' + item.title);
-		if (item.title.length > 14) {
-			item.length = item.title.length * 0.7;
-		} else {
-			item.length = item.title.length * 0.8;
-		}
-	});
-
-	const loadData = JSON.parse(JSON.stringify(data));
-	return {
-		database: loadData
-	};
+	// console.log('>>>> start');
+	// const data = await database
+	// 	.find(
+	// 		{},
+	// 		{
+	// 			projection: {
+	// 				_id: String,
+	// 				title: String,
+	// 				state: Number
+	// 			}
+	// 		}
+	// 	)
+	// 	.toArray();
+	// data.forEach((item) => {
+	// 	console.log('>>>> title : ' + item.title);
+	// 	if (item.title.length > 14) {
+	// 		item.length = item.title.length * 0.7;
+	// 	} else {
+	// 		item.length = item.title.length * 0.8;
+	// 	}
+	// });
+	// const loadData = JSON.parse(JSON.stringify(data));
+	// return {
+	// 	database: loadData
+	// };
 };
