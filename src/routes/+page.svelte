00d5fd324b3feefd26c9a6d5e760b14883267ff8
Hyeyeon Seo <42148741+skojphy@users.noreply.github.com>
2023-09-24 04:12:02 +0900

    :construction: 프로젝트 세팅 중

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,25 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import welcome from '$lib/images/svelte-welcome.webp';
+	import welcome_fallback from '$lib/images/svelte-welcome.png';
+
+	import { onMount } from 'svelte';
+
+	export let data;
+	$: ({ tutorials } = data);
+
+	data.tutorials.forEach((item) => {
+		console.log('>>>> ' + item.title);
+	});
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="아보하 흑백전" />
+</svelte:head>
+
+<section>
+	<h2>흑백전ㅋㅋ</h2>
+</section>
+
+<style>
+</style>
