af5703c7d1fa635add8e58c8083420cd6351db7c
Hyeyeon Seo <42148741+skojphy@users.noreply.github.com>
2023-09-30 03:42:35 +0900

    :sparkles: 카카오맵 API 연결

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,41 @@
 <script>
+	import './styles.css';
 	import poster from '$lib/images/poster.jpeg';
+	import { onMount } from 'svelte';
 	const today = new Date();
 	const eventDay = new Date('2023-10-08');
 	const diff = eventDay - today;
 	const diffDay = Math.floor(diff / (1000 * 60 * 60 * 24));
+
+	onMount(() => {
+		const [lat, lng] = [37.504547, 126.897091];
+		const container = document.getElementById('map');
+		const markerPosition = new kakao.maps.LatLng(lat, lng);
+
+		const options = {
+			center: markerPosition,
+			level: 3
+		};
+
+		const map = new kakao.maps.Map(container, options);
+
+		const zoomControl = new kakao.maps.ZoomControl();
+
+		const marker = new kakao.maps.Marker({
+			position: markerPosition
+		});
+		const content =
+			'<div class ="label" style="transform: translateY(-55px); background-color: black; padding: 2px 3px; border-radius: 8px;"><span class="left"></span><span class="center" style="color: white">4층 D2 파티룸!</span><span class="right"></span></div>';
+
+		const customOverlay = new kakao.maps.CustomOverlay({
+			position: markerPosition,
+			content: content
+		});
+
+		map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
+		marker.setMap(map);
+		customOverlay.setMap(map);
+	});
 </script>
 
 <main>
@@ -16,6 +48,9 @@
 			<p>평점 5/5</p>
 			<p>D-{diffDay}</p>
 		</div>
+
+		<div id="map" style="width:80%;height:300px;" />
+
 		<a href="/reservation">나도 참전하기</a>
 	</div>
 </main>
