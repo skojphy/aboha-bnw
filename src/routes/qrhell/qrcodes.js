1096e7b5cef9c9e46d4a301a76ac1d1aeb0201b8
Hyeyeon Seo <42148741+skojphy@users.noreply.github.com>
2023-10-05 05:29:02 +0900

    :card_file_box: qrcode.js 필드 추가

--- src/routes/qrhell/qrcodes.js
+++ src/routes/qrhell/qrcodes.js
@@ -1,150 +1,180 @@
 export const codes = [
 	{
+		no: 1,
 		id: 'hrjdjf07',
 		desc: 'hrjdjf07!',
-		isSuccess: false
+		isSuccess: true
 	},
 	{
+		no: 2,
 		id: 'dikur0ug',
 		desc: 'dikur0ug!',
-		isSuccess: false
+		isSuccess: true
 	},
 	{
+		no: 3,
 		id: 'ikxxb0eh',
 		desc: 'ikxxb0eh!',
-		isSuccess: false
+		isSuccess: true
 	},
 	{
+		no: 4,
 		id: 'e3nirieb',
 		desc: 'e3nirieb!',
-		isSuccess: false
+		isSuccess: true
 	},
 	{
+		no: 5,
 		id: 'roxwxxbv',
 		desc: 'roxwxxbv!',
-		isSuccess: false
+		isSuccess: true
 	},
 	{
+		no: 6,
 		id: 'sbg239tj',
 		desc: 'sbg239tj!',
-		isSuccess: false
+		isSuccess: true
 	},
 	{
+		no: 7,
 		id: 'ruhie73y',
 		desc: 'ruhie73y!',
-		isSuccess: false
+		isSuccess: true
 	},
 	{
+		no: 8,
 		id: '6wq3i6ki',
 		desc: '6wq3i6ki!',
-		isSuccess: false
+		isSuccess: true
 	},
 	{
+		no: 9,
 		id: '932db3g1',
 		desc: '932db3g1!',
-		isSuccess: false
+		isSuccess: true
 	},
 	{
+		no: 10,
 		id: 'yxn57tl3',
 		desc: 'yxn57tl3!',
-		isSuccess: false
+		isSuccess: true
 	},
 	{
+		no: 11,
 		id: 'fnwjkq91',
 		desc: 'fnwjkq91!',
-		isSuccess: false
+		isSuccess: true
 	},
 	{
+		no: 12,
 		id: 'qgdswryt',
 		desc: 'qgdswryt!',
-		isSuccess: false
+		isSuccess: true
 	},
 	{
+		no: 13,
 		id: 'did8mck5',
 		desc: 'did8mck5!',
-		isSuccess: false
+		isSuccess: true
 	},
 	{
+		no: 14,
 		id: 'o10gp5c8',
 		desc: 'o10gp5c8!',
-		isSuccess: false
+		isSuccess: true
 	},
 	{
+		no: 15,
 		id: 'o9zaqg43',
 		desc: 'o9zaqg43!',
-		isSuccess: false
+		isSuccess: true
 	},
 	{
+		no: 16,
 		id: 'wyhge7zc',
 		desc: 'wyhge7zc!',
 		isSuccess: false
 	},
 	{
+		no: 17,
 		id: '3xdhshpp',
 		desc: '3xdhshpp!',
 		isSuccess: false
 	},
 	{
+		no: 18,
 		id: 'qjaa506i',
 		desc: 'qjaa506i!',
 		isSuccess: false
 	},
 	{
+		no: 19,
 		id: '6v5ja1z8',
 		desc: '6v5ja1z8!',
 		isSuccess: false
 	},
 	{
+		no: 20,
 		id: '3ws4m07z',
 		desc: '3ws4m07z!',
 		isSuccess: false
 	},
 	{
+		no: 21,
 		id: 'fnk2sszx',
 		desc: 'fnk2sszx!',
-		isSuccess: false
+		isSuccess: true
 	},
 	{
+		no: 22,
 		id: 'wtjaz82t',
 		desc: 'wtjaz82t!',
-		isSuccess: false
+		isSuccess: true
 	},
 	{
+		no: 23,
 		id: 'fk443xoh',
 		desc: 'fk443xoh!',
-		isSuccess: false
+		isSuccess: true
 	},
 	{
+		no: 24,
 		id: '2mihec5v',
 		desc: '2mihec5v!',
-		isSuccess: false
+		isSuccess: true
 	},
 	{
+		no: 25,
 		id: '8fzbrovf',
 		desc: '8fzbrovf!',
-		isSuccess: false
+		isSuccess: true
 	},
 	{
+		no: 26,
 		id: 'f8v4hdni',
 		desc: 'f8v4hdni!',
-		isSuccess: false
+		isSuccess: true
 	},
 	{
+		no: 27,
 		id: 'juvdw2n0',
 		desc: 'juvdw2n0!',
 		isSuccess: false
 	},
 	{
+		no: 28,
 		id: 'x1hcvjl7',
 		desc: 'x1hcvjl7!',
 		isSuccess: false
 	},
 	{
+		no: 29,
 		id: 'scd316k4',
 		desc: 'scd316k4!',
 		isSuccess: false
 	},
 	{
+		no: 30,
 		id: 'qk2ztfwd',
 		desc: 'qk2ztfwd!',
 		isSuccess: false
