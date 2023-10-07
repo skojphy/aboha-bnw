17345c32738e9ca9761850d9336faa6af5378659
Hyeyeon Seo <42148741+skojphy@users.noreply.github.com>
2023-10-08 05:26:24 +0900

    :sparkles: prize 페이지 추가

---
+++ src/routes/qrhell/prize5959/+page.svelte
@@ -0,0 +1,23 @@
+<main>
+	<h1>축하합니다.</h1>
+	<p>당신은 해적왕!</p>
+	<p>qr코드를 완성했다면 쌈장에게 이 화면을 캡쳐하여 보내 주세요.</p>
+</main>
+
+<style>
+	main {
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+		width: 100%;
+		margin: 0 auto;
+		box-sizing: border-box;
+		background-color: white;
+		color: black;
+		text-align: center;
+		font-size: 30px;
+	}
+	p {
+		margin: 30px;
+	}
+</style>
